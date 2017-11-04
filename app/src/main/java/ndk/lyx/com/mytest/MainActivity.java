7bdc631b342dec652fdb432ea28b56c9b78bc402
lyx <lyx123>
2017-11-04 12:52:09 +0800

    Initial commit

---
+++ app/src/main/java/ndk/lyx/com/mytest/MainActivity.java
@@ -0,0 +1,48 @@
+package ndk.lyx.com.mytest;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String[] strings = new String[1];
+        strings[0]= Manifest.permission.CAMERA;
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(strings, 601);
+                Toast.makeText(MainActivity.this,"正在开启了",Toast.LENGTH_LONG).show();
+            }
+        }else {
+            Toast.makeText(MainActivity.this,"已经开启",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 601) {
+            for (int i = 0; i < grantResults.length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this,"成功开启",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this,"失败开启",Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+}
